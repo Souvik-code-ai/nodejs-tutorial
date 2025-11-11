6a546a75b6836e66889d567cdf865cdf2b4f4d15
SOUVIK PURKAIT <purkaitsouvik531@gmail.com>
2025-11-11 18:37:49 +0530

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-//const express = require('express');
-const mongoose = require('mongoose');
-const database_connect = require('./database_connection');
-const http_request=require('./http_requests');
-module.exports=http_request;
-
