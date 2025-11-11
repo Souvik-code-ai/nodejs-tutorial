847eeb618af7baf25e0971d4b819fd76f0853721
SOUVIK PURKAIT <purkaitsouvik531@gmail.com>
2025-11-11 18:36:18 +0530

    Delete database_connection.js

--- database_connection.js
+++
@@ -1,6 +0,0 @@
-let mongoose=require('mongoose');
-// MongoDB connection
-let database_connect=mongoose.connect("mongodb+srv://purkaitsouvik_531:upgXaRGKU1alQs3S@cluster0.tljrxdk.mongodb.net/?appName=Cluster0")
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-module.exports=database_connect;
